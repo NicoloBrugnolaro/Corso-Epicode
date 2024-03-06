faf4fa31fa9c93b25f59a325bfb14e0e29cdb056
NicoloBrugnolaro <nicolo.brugnolaro@gmail.com>
2024-03-06 16:42:59 +0100

    esercitazione programmazione C

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 4;
+    int risultato = num1 * num2;
+    
+    printf("Il risultato della moltiplicazione di %d e %d è %d\n", num1, num2, risultato);
+    
+    return 0;
+}
